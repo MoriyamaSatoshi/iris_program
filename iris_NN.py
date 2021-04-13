868571f9afc78180a217a3ca0a70654524d0a75d
MoriyamaSatoshi <82300501+MoriyamaSatoshi@users.noreply.github.com>
2021-04-13 14:58:56 +0900

    Update iris_NN.py

--- iris_NN.py
+++ iris_NN.py
@@ -1,31 +1,33 @@
 #日本大学理工学部応用情報工学科 森山慧
 #iris.csvファイルをニューラルネットワークで学習させるプログラム
+#特徴量を'gakuhen_nagasa','gakuhen_haba','hanabira_nagasa','hanabira_haba'としてirsiの種類を判別する
+#言語 Python
 
 import time
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 #ニューラルネットワークの作成
 model=MLPClassifier(hidden_layer_sizes=(100,100),activation='logistic',solver='adam',max_iter=1000)
 df = pd.read_csv('C:/ゼミナール活動/iris.csv')
 
+#iris.csvファイルの取り込み
 col1 = ['gakuhen_nagasa','gakuhen_haba','hanabira_nagasa','hanabira_haba']
 x = df[col1]
 col2=['syurui']
 y = df[col2]
 
-#テストデータと訓練データ分割
+#テストデータと訓練データを分割
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
 
-#時間計測
+#時間計測(開始)
 start_time = time.perf_counter()
 
-#学習
+#ニューラルネットワークによる学習
 model.fit(x_train,y_train.values.ravel())
 
-#時間測定
+#時間計測(終了)
 elapsed_time = time.perf_counter()-start_time
 print("実行時間[s]　"+str(elapsed_time))
 
